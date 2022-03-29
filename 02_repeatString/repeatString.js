16bf7ed023c7100413b53c537428361445d374a7
TinyBossofaMilk <ppcctan@gmail.com>
2022-03-29 00:37:35 -0700

    repeatString.js done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(repeatWord, numRepeats) {
+    let output = "";
 
+    if(numRepeats < 0)
+        {return "ERROR";}
+    for(let i = 0; i < numRepeats; i++)
+        {output += repeatWord;}
+    return output;
 };
 
 // Do not edit below this line
