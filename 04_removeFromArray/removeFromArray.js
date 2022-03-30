26e11b3e355c3dc30ca7add16ecea0d20ee69607
TinyBossofaMilk <ppcctan@gmail.com>
2022-03-30 01:00:24 -0700

    finished 04 javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(numsArr, numToRemove, ...moreNumstoRemove) {
 
+    while(moreNumstoRemove.length > 0)
+    {
+        //let toRemove = moreNumstoRemove.pop();
+
+        //if(typeof toRemove == Number)
+        numsArr = removeFromArray(numsArr,moreNumstoRemove.pop());
+    }
+    //const output = numsArr.filter( (num, index, ntr =numToRemove) => {num != ntr;});
+    const output = [];
+
+    for(num of numsArr)
+    {
+        if(num === numToRemove)
+            {}
+        else
+            {output.push(num);}
+    }
+
+    return output;
 };
 
 // Do not edit below this line
